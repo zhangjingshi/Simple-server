c824d477a4a8b28cfec39d6288f20f0856eb6c3f
zhangjingshi <83528694@qq.com>
2018-12-18 13:14:17 +0800

    第一次提交

---
+++ server.js
@@ -0,0 +1,52 @@
+ar http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('HTTP 路径为/n' + path)
+  if(path == '/style'){
+      response.setHeader('Content-Type', 'text/style ; charset=utf-8')
+      response.write('baby{background-color; #black;}h1{color:green;}')
+      response.end()
+  }else if(path == 'script'){
+     response.setHeader('Content-Type', 'text/style ; charset=utf-8')
+     response.write('alert("弹出JS BBBBBBBBBBBBBBBBB")')
+     response.end()
+  }else if(path == 'index'){
+     response.setHeader('Content-Type', 'text/style ; charset=utf-8')
+     response.write('<！DOCTYPE>/n<html>' +
+                     '<head><link rel="stylesheet" href= "/style">' + 
+                     '</head><bady>' +
+                     '<h1>还是得多学多看哦 </h1> ' + 
+                     '<script src="/script"></script>' + 
+                     '</bady></html>')
+     response.end()
+  }eles{
+      response.statusCode = 404
+      response.end()
+  } 
+
+
+
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
